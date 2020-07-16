e94f56f48cd2f133a5ad7fc031c27c6a445e8c71
AlexUzan <alexandreuzan.prepa@gmail.com>
2020-07-16 10:46:27 +0200

    Basic config (eslint + ts + yarn)

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,11 +46,12 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 0,
         '@typescript-eslint/camelcase': 0,
         '@typescript-eslint/no-inferrable-types': 0,
+        '@typescript-eslint/ban-ts-comment': 0,
 
         // react
         'react/no-did-mount-set-state': 0,
-        "react-hooks/rules-of-hooks": 2, // Checks rules of Hooks
-        "react-hooks/exhaustive-deps": 1, // Checks effect dependencies
+        'react-hooks/rules-of-hooks': 2, // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 1, // Checks effect dependencies
 
         // react-native
         'react-native/no-inline-styles': 0,
