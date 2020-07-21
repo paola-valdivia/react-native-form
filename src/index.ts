fc7225318f156b0765a8ecd58a70db3b9f7fb051
AlexUzan <alexandreuzan.prepa@gmail.com>
2020-07-21 10:44:39 +0200

    Remove InstructionField

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import DateField from './Fields/DateField';
-import InstructionField from './Fields/InstructionField';
 import MCQField from './Fields/MCQField';
 import MultiLineTextField from './Fields/MultiLineTextField';
 import NumericTextField from './Fields/NumericTextField';
 import PhotoField from './Fields/PhotoField';
 import SingleLineTextField from './Fields/SingleLineTextField';
+import Description from './components/Description';
 
-export { DateField, InstructionField, MCQField, MultiLineTextField, NumericTextField, PhotoField, SingleLineTextField };
+export { DateField, MCQField, MultiLineTextField, NumericTextField, PhotoField, SingleLineTextField, Description };
