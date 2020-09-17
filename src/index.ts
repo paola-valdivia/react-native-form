6b5484517f736939b8bea37e2272cffa6f3104c9
AlexUzan <alexandreuzan.prepa@gmail.com>
2020-09-17 17:40:42 +0200

    Remove NumericTextField

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import DateField from './Fields/DateField';
 import MCQField from './Fields/MCQField';
 import MultiLineTextField from './Fields/MultiLineTextField';
-import NumericTextField from './Fields/NumericTextField';
 import PhotoField from './Fields/PhotoField';
 import SingleLineTextField from './Fields/SingleLineTextField';
 import Description from './components/Description';
 
-export { DateField, MCQField, MultiLineTextField, NumericTextField, PhotoField, SingleLineTextField, Description };
+export { DateField, MCQField, MultiLineTextField, PhotoField, SingleLineTextField, Description };
