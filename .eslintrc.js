63e40d0a92934a2d8c74582095eb4cae16568b03
AlexUzan <alexandreuzan.prepa@gmail.com>
2020-09-24 18:16:13 +0200

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,59 +1,39 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-        'react',
-        'react-native',
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        '@react-native-community',
-        'prettier',
-        'prettier/@typescript-eslint',
-        'prettier/react',
-    ],
     parserOptions: {
-        ecmaVersion: 2020,
         sourceType: 'module',
         ecmaFeatures: {
             jsx: true,
         },
     },
-    // 0 = 'off', 1 = 'warn', 2 = 'error'
+    plugins: ['@typescript-eslint', 'react', 'react-native'],
+    extends: [
+        'eslint:recommended',
+        '@react-native-community',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/@typescript-eslint',
+        'prettier/react',
+    ],
     rules: {
         'comma-dangle': ['warn', 'only-multiline'],
-        'radix': 0,
-
-        // prettier
+        radix: 0,
+        // Prettier
         'prettier/prettier': [
             'warn',
             {
-                arrowParens: "always",
+                arrowParens: 'always',
+                bracketSpacing: true,
+                singleQuote: true,
+                trailingComma: 'all',
                 tabWidth: 4,
                 printWidth: 120,
-                singleQuote: true,
-                trailingComma: "es5",
-            }
+            },
         ],
-
-        //typescript
-        '@typescript-eslint/no-non-null-assertion': 0,
-        '@typescript-eslint/explicit-function-return-type': 0,
-        '@typescript-eslint/ban-ts-ignore': 0,
-        '@typescript-eslint/no-explicit-any': 0,
-        '@typescript-eslint/camelcase': 0,
-        '@typescript-eslint/no-inferrable-types': 0,
-        '@typescript-eslint/ban-ts-comment': 0,
-
-        // react
-        'react/no-did-mount-set-state': 0,
-        'react-hooks/rules-of-hooks': 2, // Checks rules of Hooks
-        'react-hooks/exhaustive-deps': 1, // Checks effect dependencies
-
-        // react-native
-        'react-native/no-inline-styles': 0,
+        // Typescript
+        '@typescript-eslint/no-use-before-define': 0,
+        '@typescript-eslint/ban-types': 1,
     },
 };
