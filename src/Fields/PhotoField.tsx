93a202c78a990eee6009e8e03bfbae93e2df89f4
adamsatouri <adamsatouri@gmail.com>
2021-08-31 12:03:15 +0200

    fixup! [CROWD-165] make-labels-multiline-on-PhotoField

--- src/Fields/PhotoField.tsx
+++ src/Fields/PhotoField.tsx
@@ -43,7 +43,7 @@ function PhotoField(props: PhotoFieldProps) {
                     props.labelAndValidationContainerStyle,
                 ]}
             >
-                <Text style={[sharedStyles.labelText, props.labelStyle]}>
+                <Text numberOfLines={3} ellipsizeMode={'tail'} style={[sharedStyles.labelText, props.labelStyle]}>
                     {props.label}
                 </Text>
                 <ValidationDot isValid={props.isValid} style={props.validationDotStyle} colors={props.colors} />
